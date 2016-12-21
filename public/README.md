0da79a0fa80937c080f43b3612d330f3d3ae3aba
20101777 <robinwilkieglasgow@hotmail.com>
2016-12-21 09:59:06 +0000

    deployed to firebase

---
+++ public/README.md
@@ -0,0 +1,2 @@
+# Learn JavaScript
+This repository has  been created for the javascript tutorial for Software development for the WWW and Developing websites for multi platforms
---
+++ public/javascript/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by davem on 27/11/2016.
+ */
+'use strict';
+var learnjavascript={};
+
+learnjavascript.questionView = function(problemNumber) {
+    var title = 'Question #' + problemNumber + 'Arriving Soon!!';
+    return $('<div class="question-view">').text(title);
+};
+
+learnjavascript.showView = function(hash){
+    var routes = {
+        '#question': learnjavascript.questionView
+    };
+
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn(hashParts[1]));
+    }
+};
+
+learnjavascript.appOnReady = function(){
+    window.onhashchange = function(){
+    learnjavascript.showView(window.location.hash);
+};
+    learnjavascript.showView(window.location.hash);
+};
